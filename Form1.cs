7f1fb027671c0b04182d7d656dedc4cb02f24785
Nullsrc <beallen529@gmail.com>
2017-01-20 19:37:29 -0600

    Draw pieces onto board

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,15 @@ namespace Reversi
         int x;
         int y;
         int cellSize;
+        int row;
+        int col;
+        int turn = 0;
+        int cursorCol;
+        int cursorRow;
+        int[,] board = new int[8, 8] { { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, 
+                                       { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, 0, 1, -1, -1, -1 }, 
+                                       { -1, -1, -1, 1, 0, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 }, 
+                                       { -1, -1, -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1, -1, -1 } };
 
         public Form1()
         {
@@ -27,10 +36,33 @@ namespace Reversi
             this.MaximumSize = new System.Drawing.Size(1020, 1020);
         }
 
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            cursorCol = (int)Math.Floor((e.X - x) * 1.0 / cellSize);
+            cursorRow = (int)Math.Floor((e.Y - y) * 1.0 / cellSize);
+            Refresh();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            col = (int)Math.Floor((e.X - x) * 1.0 / cellSize);
+            row = (int)Math.Floor((e.Y - y) * 1.0 / cellSize);
+            if ((col < 8 && col >= 0) && (row < 8 && row >= 0))
+            {
+                if (board[col, row] == -1)
+                {
+                    board[col, row] = turn % 2;
+                    turn++;
+                }
+            }
+            Refresh();
+        }
+
+
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
-
+            
             UpdateSize();
             Refresh();
         }
@@ -62,9 +94,12 @@ namespace Reversi
                         e.Graphics.FillRectangle(Brushes.LightGray, rect);
                     }
                     else e.Graphics.FillRectangle(Brushes.Gray, rect);
+                    if (i == cursorCol && j == cursorRow) e.Graphics.FillRectangle(Brushes.Yellow, rect);
                     e.Graphics.DrawRectangle(Pens.Black, rect);
                     System.Drawing.Font font = new System.Drawing.Font("Ubuntu", cellSize / 4 * 100 / 96);
                     System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+                    if (board[i, j] == 0) e.Graphics.FillEllipse(Brushes.Blue, x + (i * cellSize) + cellSize/8, y + (j * cellSize) + cellSize/8, 3 * cellSize /4, 3 * cellSize /4);
+                    if (board[i, j] == 1) e.Graphics.FillEllipse(Brushes.Orange, x + (i * cellSize) + cellSize / 8, y + (j * cellSize) + cellSize / 8, 3 * cellSize / 4, 3 * cellSize / 4);
                 }
             }
         }
